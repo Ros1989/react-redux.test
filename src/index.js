208969dd9ed72b962a02341ebd02447398828856
Rostyslav Zhabinskyi <zhabinskiirostislav@gmail.com>
2021-05-10 16:00:47 +0300

    added app on React, Redux, Redux-Saga

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import {compose, createStore, applyMiddleware}  from 'redux'
+import { rootReducer } from './redux/rootReducer'
+import thunk from 'redux-thunk'
+import { forbiddenWordsMiddleware } from './redux/middleware'
+import createSagaMiddleWare from 'redux-saga'
+import {sagaWatcher} from './redux/sagas'
+
+const saga = createSagaMiddleWare()
+
+
+const store = createStore(rootReducer, compose(
+  applyMiddleware(
+    thunk, forbiddenWordsMiddleware, saga
+  ),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+saga.run(sagaWatcher)
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
